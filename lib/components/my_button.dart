acaaa86caa7f78d8ec11f79ebb0e204d074a0e80
Francisco Javier Diaz <142125150+Franciscooxz@users.noreply.github.com>
2024-02-05 10:35:12 -0500

    Login

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -13,12 +13,12 @@ class MyButton extends StatelessWidget {
         padding: const EdgeInsets.all(25),
         margin: const EdgeInsets.symmetric(horizontal: 25),
         decoration: BoxDecoration(
-          color: Colors.black,
+          color: const Color.fromARGB(255, 0, 0, 0),
           borderRadius: BorderRadius.circular(8),
         ),
         child: const Center(
           child: Text(
-            "Sign In",
+            "Inciar Sesion",
             style: TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.bold,
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:astroguide_flutter/components/my_button.dart';
 import 'package:astroguide_flutter/components/my_textfield.dart';
-import 'package:flutter/material.dart';
-
-
-
+import 'package:astroguide_flutter/components/square_tile.dart';
 
 class LoginPage extends StatelessWidget {
-   LoginPage ({super.key});
+  LoginPage({super.key});
 
   // text editing controllers
   final usernameController = TextEditingController();
@@ -15,66 +13,60 @@ class LoginPage extends StatelessWidget {
   // sign user in method
   void signUserIn() {}
 
-
   @override
-  Widget build  (BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black12,
-      body: Column(children: [
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 50),
 
-        //logo
-        Icon(
-          Icons.lock,
-          size: 100,
-          ), 
+              // logo
+              const Icon(
+                Icons.lock,
+                size: 100,
+              ),
 
-           const SizedBox(height: 50),
+              const SizedBox(height: 50),
 
-
-        //Welcome back
-        Text(
-                'Welcome back you\'ve been missed!',
+              Text(
+                'Bienvenido a este basto universo ',
                 style: TextStyle(
                   color: Colors.grey[700],
                   fontSize: 16,
                 ),
               ),
 
-               const SizedBox(height: 25),
+              const SizedBox(height: 25),
 
-         
-        //username textfield
-         
               MyTextField(
                 controller: usernameController,
-                hintText: 'Username',
+                hintText: 'Usuario',
                 obscureText: false,
               ),
 
               const SizedBox(height: 10),
 
-        //password textfield
-
-         MyTextField(
+              // password textfield
+              MyTextField(
                 controller: passwordController,
-                hintText: 'Password',
+                hintText: 'Contraseña',
                 obscureText: true,
               ),
 
               const SizedBox(height: 10),
 
-
-
-
-        //forgot password?
-
-         Padding(
+              // forgot password?
+              Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Text(
-                      'Forgot Password?',
+                      'olvidaste tu contraseña?',
                       style: TextStyle(color: Colors.grey[600]),
                     ),
                   ],
@@ -83,15 +75,15 @@ class LoginPage extends StatelessWidget {
 
               const SizedBox(height: 25),
 
-        //sign in button
-         MyButton(
+              // sign in button
+              MyButton(
                 onTap: signUserIn,
               ),
 
               const SizedBox(height: 50),
 
-        //or continue with google 
-           Padding(
+              // or continue with
+              Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Row(
                   children: [
@@ -104,7 +96,7 @@ class LoginPage extends StatelessWidget {
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 10.0),
                       child: Text(
-                        'Or continue with',
+                        'O continuar con',
                         style: TextStyle(color: Colors.grey[700]),
                       ),
                     ),
@@ -120,22 +112,44 @@ class LoginPage extends StatelessWidget {
 
               const SizedBox(height: 50),
 
-        
-        
+              // google + apple sign in buttons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  // google button
+                  SquareTile(imagePath: 'lib/images/google.png'),
 
+                  SizedBox(width: 25),
 
+                  // apple button
+                  SquareTile(imagePath: 'lib/images/facebook.png')
+                ],
+              ),
 
-        
+              const SizedBox(height: 50),
 
-        // Register now 
-         
-
-
-
-
-
-      ]),
+              // not a member? register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'No tienes una cuenta?',
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                  const SizedBox(width: 4),
+                  const Text(
+                    'Registrate Ahora',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
-
   }
 }
